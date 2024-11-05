a99c01f7a6fffb088e6863ec6306100cb9398348
jayel888 <15644@coderacademy.edu.au>
2024-11-05 19:50:44 +1100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-function funcA(){
-    // fetch some data from API
-    // do something
-}
+console.log("this file is running!")
 
-function funcB() {
-    // 
-    // 
-}
-
-funcA();
-funcB();
+let defaultEnv = {
+    PORT: 5050,
+    DATABASE_URI: "asdfasdf:asdf",
+    JWT_SECURITY_KEY: "fsdfs",
+    SECRET_KEY: "apple",
+    SECURE_API_KEY: "sdfasdfa34534"
+};
