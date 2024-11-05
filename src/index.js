e2278f758d784fc71a36f16cbb3d543a2248fe19
jayel888 <15644@coderacademy.edu.au>
2024-11-05 20:06:25 +1100

    feat: file write using both sync and async modes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-console.log("this file is running!")
+console.log("This file is running!");
 
 let defaultEnv = {
     PORT: 5050,
-    DATABASE_URI: "asdfasdf:asdf",
-    JWT_SECURITY_KEY: "fsdfs",
+    DATABASE_URI: "asdasd:asdasd",
+    JWT_SECURITY_KEY: "asdasdas",
     SECRET_KEY: "apple",
-    SECURE_API_KEY: "sdfasdfa34534"
+    SECURE_API_KEY: "sdfas76d5f7as6d5fasd"
 };
+
+let contentToWrite = "";
+
+// What it should look like:
+// PORT=5050
+// DATABASE_URI="asdasd:asdasd"
+// JWT_SECURITY_KEY="asdasdas"
+// SECRET_KEY="apple"
+// SECURE_API_KEY="sdfas76d5f7as6d5fasd"
+
+Object.keys(defaultEnv).forEach(
+    envKey => {
+        // Add a line to the contentToWrite variable with the key name and value
+        contentToWrite += `${envKey}=${defaultEnv[envKey]}\n`;
+    }
+);
+
+// Give us an idea of what get stored in the file
+console.log(contentToWrite);
+
+// All the file handling operations will be performed using fs
+const fs = require("node:fs");
+
+// Synchronous way
+// fs.writeFileSync(".env", contentToWrite);
+
+// Asynchronous way
+// fs.writeFile(filepath, fileCOntents, callback);
+fs.writeFile(".env", contentToWrite, (error) => {
+    if (error){
+        console.log("Error encountered: ", error.message);
+    }
+    else {
+        console.log("File written successfully");
+    }
+});
+
+console.log("File has been written.")
